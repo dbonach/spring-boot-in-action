db689d4d3da4907fa9a0cf4b21cca6e2ea70027f
dbonach <deivitlopes@gmail.com>
2021-12-29 12:26:36 -0300

    Create project

---
+++ src/main/java/com/manning/springboot/SpringBootInActionApplication.java
@@ -0,0 +1,13 @@
+package com.manning.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInActionApplication.class, args);
+	}
+
+}
